5df5f8950dd9eb4c1a39206f50d66a20931126b8
Ziphil <yfe23457@nifty.com>
2025-07-25 01:54:33 +0900

    変更

--- source/util/anatomy.ts
+++ source/util/anatomy.ts
@@ -1,9 +1,19 @@
 //
 
-import {AffixType} from "../type/anatomy";
+import {Affix, AffixType, PATTERN_DATA, Pattern, PatternCategory, PatternType} from "../type/anatomy";
 
 
-export function getAffixType(affix: string): AffixType | null {
+export function getPatternCategory(pattern: Pattern): PatternCategory | null {
+  const category = PATTERN_DATA.get(pattern)?.category ?? null;
+  return category;
+}
+
+export function getPatternType(pattern: Pattern): PatternType | null {
+  const type = PATTERN_DATA.get(pattern)?.type ?? null;
+  return type;
+}
+
+export function getAffixType(affix: Affix): AffixType | null {
   if (affix.match(/^([^-]+?)-$/)) {
     return "prestem";
   } else if (affix.match(/^-(е|о)([^-]+?)-$/)) {
