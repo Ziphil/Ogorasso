44c58655faddea2a2172ea4a9e258c52063dea9b
Ziphil <yfe23457@nifty.com>
2025-08-24 14:58:01 +0900

    修正

--- source/conversion/word.ts
+++ source/conversion/word.ts
@@ -21,12 +21,13 @@ export function convertWord(rawWord: any): Word {
 }
 
 export function convertNormalWord(rawWord: any): NormalWord {
-  const rawRelations = rawWord["relations"] as Array<any>;
+  const rawSections = rawWord["sections"] as Array<any>;
+  const rawAnatomyRelations = rawSections[rawSections.length - 1]["relations"] as Array<any>;
   const word = {
     kind: "normal",
     number: +rawWord["number"],
     spelling: rawWord["spelling"],
-    anatomy: parseAnatomy(rawWord["spelling"], rawRelations),
+    anatomy: parseAnatomy(rawWord["spelling"], rawAnatomyRelations),
     sections: rawWord["sections"].map(convertSection),
     foreign: rawWord["tags"].includes("外来語")
   } satisfies NormalWord;
@@ -36,11 +37,12 @@ export function convertNormalWord(rawWord: any): NormalWord {
 export function convertRootWord(rawWord: any): RootWord {
   const root = extractRoot(rawWord["spelling"]);
   if (root !== null) {
+    const rawSections = rawWord["sections"] as Array<any>;
     const word = {
       kind: "root",
       number: +rawWord["number"],
       root,
-      equivalents: rawWord["equivalents"].map(convertEquivalent),
+      equivalents: (rawSections[0] !== undefined) ? rawSections[0]["equivalents"].map(convertEquivalent) : [],
       foreign: rawWord["tags"].includes("外来語")
     } satisfies RootWord;
     return word;
@@ -52,11 +54,12 @@ export function convertRootWord(rawWord: any): RootWord {
 export function convertPatternWord(rawWord: any): PatternWord {
   const spelling = extractPatternSpelling(rawWord["spelling"]);
   if (spelling !== null) {
+    const rawSections = rawWord["sections"] as Array<any>;
     const word = {
       kind: "pattern",
       number: +rawWord["number"],
       spelling,
-      equivalents: rawWord["equivalents"].map(convertEquivalent)
+      equivalents: (rawSections[0] !== undefined) ? rawSections[0]["equivalents"].map(convertEquivalent) : []
     } satisfies PatternWord;
     return word;
   } else {
@@ -67,11 +70,12 @@ export function convertPatternWord(rawWord: any): PatternWord {
 export function convertAffixWord(rawWord: any): AffixWord {
   const spelling = extractAffixSpelling(rawWord["spelling"]);
   if (spelling !== null) {
+    const rawSections = rawWord["sections"] as Array<any>;
     const word = {
       kind: "affix",
       number: +rawWord["number"],
       spelling,
-      equivalents: rawWord["equivalents"].map(convertEquivalent)
+      equivalents: (rawSections[0] !== undefined) ? rawSections[0]["equivalents"].map(convertEquivalent) : []
     } satisfies AffixWord;
     return word;
   } else {
