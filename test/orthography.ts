b8c8ad08e1fd49671c67fbc88f3022cd9f061ad9
Ziphil <yfe23457@nifty.com>
2026-05-17 23:15:12 +0900

    テスト機構を追加

---
+++ test/orthography.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import {describe, expect, test} from "vitest";
+import {convertCyrillicToLatin} from "../source";
+
+describe("orthography", () => {
+  test("convert cyrillic to latin", () => {
+    expect(convertCyrillicToLatin("еди̂лат")).toBe("edîlat");
+    expect(convertCyrillicToLatin("жу̂кке")).toBe("žûkke");
+    expect(convertCyrillicToLatin("коттоси̂чло")).toBe("kottosîčlo");
+    expect(convertCyrillicToLatin("беббоди̂жжамо")).toBe("bebbodîžžamo");
+  });
+});
