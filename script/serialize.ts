5796381b82c26acc8e17a5903ef6e4826469dd20
Ziphil <yfe23457@nifty.com>
2026-05-20 20:32:54 +0900

    変更

--- script/serialize.ts
+++ script/serialize.ts
@@ -1,15 +1,18 @@
 //
 
 import fs from "fs/promises";
+import {parseArgs} from "node:util";
 import {readEntries, writeEntries} from "../source";
 
 
 async function execute(): Promise<void> {
-  const name = "5232";
-  const input = await fs.readFile(`script/file/${name}.zpdc`, "utf8");
+  const {values} = parseArgs({options: {
+    name: {type: "string", short: "n"}
+  }});
+  const input = await fs.readFile(`script/file/${values.name}.zpdc`, "utf8");
   const entries = readEntries(input);
   const output = JSON.stringify(writeEntries(entries), null, 2);
-  await fs.writeFile(`script/file/${name}.fndc`, output);
+  await fs.writeFile(`script/file/${values.name}.fndc`, output);
 }
 
 execute();
